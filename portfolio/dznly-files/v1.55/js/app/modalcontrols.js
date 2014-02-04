422ac4c77ecb30e05fba963ebff673018d13bd01
Patrick Conway <conway.pd@gmail.com>
2014-02-03 18:39:40 -0800

    compile

--- portfolio/dznly-files/v1.55/js/app/modalcontrols.js
+++
@@ -1,8 +0,0 @@
-//To add additional modal controls, add them to the dependencies here
-define([
-    'app/views/filepicker',
-    'app/views/embedder'
-    ], 
-    function(FilePickerControl, EmbedControl) {
-    return arguments;
-});
